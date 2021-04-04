0167f414f32e3d50deef9460c4886e9076d58144
yl3030 <yl301114@gmail.com>
2021-04-05 03:19:19 +0800

    0405

---
+++ public/js/index.js
@@ -0,0 +1,91 @@
+// menu
+$(".menu").click(function(){
+	$("header").children("div").children("nav").slideDown(300);
+})
+$(".menu-close").click(function(){
+	$("header").children("div").children("nav").slideUp(300);
+})
+
+// 輪播圖
+var Rswiper = new Swiper("#banner", {
+	loop: true,
+	speed: 600,
+	parallax: true,
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: "#banner-pag",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".Snext", // 上一頁按鈕物件
+		prevEl: ".Sprev", // 下一頁按鈕物件
+	},
+});
+// 滑鼠滑入後停止輪播
+$("#banner").mouseenter(function () {
+	Rswiper.autoplay.stop();
+});
+$("#banner").mouseleave(function () {
+	Rswiper.autoplay.start();
+});
+
+// 輪播圖
+var Bswiper = new Swiper(".blog-banner", {
+	loop: true,
+	slidesPerView: 4,
+	spaceBetween: 0,
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+	// pagination: {
+	// 	el: "#blog-pag",
+	// 	clickable: true,
+	// },
+	// navigation: {
+	// 	nextEl: ".Bnext",
+	// 	prevEl: ".Bprev", 
+	// },
+});
+// 滑鼠滑入後停止輪播
+$(".blog-banner").mouseenter(function () {
+	Bswiper.autoplay.stop();
+});
+$(".blog-banner").mouseleave(function () {
+	Bswiper.autoplay.start();
+});
+
+// 輪播圖
+var Nswiper = new Swiper("#new", {
+	loop: true,
+	slidesPerView: 1,
+	direction: 'vertical',
+	// spaceBetween: 0,
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+	// pagination: {
+	// 	el: "#blog-pag",
+	// 	clickable: true,
+	// },
+	navigation: {
+		nextEl: ".Nnext",
+		prevEl: ".Nprev", 
+	},
+});
+// 滑鼠滑入後停止輪播
+$(".blog-banner").mouseenter(function () {
+	Bswiper.autoplay.stop();
+});
+$(".blog-banner").mouseleave(function () {
+	Bswiper.autoplay.start();
+});
+
+// $(".close-btn").click(function(){
+// 	$("#ModalAdd").css("display","none");
+// 	$(".modal-backdrop").hide();
+// })
