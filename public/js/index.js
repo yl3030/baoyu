40a2f2ab0e75a6100c9aeda609c6171ebd1dae64
yl301114 <yl301114@gmail.com>
2021-06-10 02:05:22 +0800

    0610

--- public/js/index.js
+++ public/js/index.js
@@ -1,97 +1,113 @@
 // menu
-$(".menu").click(function(){
-	$("header").children("div").children("nav").slideDown(300);
-})
-$(".menu-close").click(function(){
-	$("header").children("div").children("nav").slideUp(300);
-})
+$(".menu").click(function () {
+  $("header").children("div").children("nav").slideDown(300);
+});
+$(".menu-close").click(function () {
+  $("header").children("div").children("nav").slideUp(300);
+});
 
 // 單選
-$(".single-choice").children("div").click(function(){
-	$(".single-choice").children("div").removeClass("active");
-	$(this).addClass("active");
-})
+$(".single-choice")
+  .children("div")
+  .click(function () {
+    $(".single-choice").children("div").removeClass("active");
+    $(this).addClass("active");
+  });
 
 // 輪播圖
 var Rswiper = new Swiper("#banner", {
-	loop: true,
-	speed: 600,
-	parallax: true,
-	autoplay: {
-		delay: 3000,
-		disableOnInteraction: false,
-	},
-	pagination: {
-		el: "#banner-pag",
-		clickable: true,
-	},
-	navigation: {
-		nextEl: ".Snext", // 上一頁按鈕物件
-		prevEl: ".Sprev", // 下一頁按鈕物件
-	},
+  loop: true,
+  speed: 600,
+  parallax: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: "#banner-pag",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".Snext", // 上一頁按鈕物件
+    prevEl: ".Sprev", // 下一頁按鈕物件
+  },
 });
 // 滑鼠滑入後停止輪播
 $("#banner").mouseenter(function () {
-	Rswiper.autoplay.stop();
+  Rswiper.autoplay.stop();
 });
 $("#banner").mouseleave(function () {
-	Rswiper.autoplay.start();
+  Rswiper.autoplay.start();
 });
 
 // 輪播圖
 var Bswiper = new Swiper(".blog-banner", {
-	loop: true,
-	slidesPerView: 4,
-	spaceBetween: 0,
-	autoplay: {
-		delay: 3000,
-		disableOnInteraction: false,
-	},
-	// pagination: {
-	// 	el: "#blog-pag",
-	// 	clickable: true,
-	// },
-	// navigation: {
-	// 	nextEl: ".Bnext",
-	// 	prevEl: ".Bprev", 
-	// },
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 0,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  // pagination: {
+  // 	el: "#blog-pag",
+  // 	clickable: true,
+  // },
+  // navigation: {
+  // 	nextEl: ".Bnext",
+  // 	prevEl: ".Bprev",
+  // },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
 });
 // 滑鼠滑入後停止輪播
 $(".blog-banner").mouseenter(function () {
-	Bswiper.autoplay.stop();
+  Bswiper.autoplay.stop();
 });
 $(".blog-banner").mouseleave(function () {
-	Bswiper.autoplay.start();
+  Bswiper.autoplay.start();
 });
 
 // 輪播圖
 var Nswiper = new Swiper("#new", {
-	loop: true,
-	slidesPerView: 1,
-	direction: 'vertical',
-	// spaceBetween: 0,
-	autoplay: {
-		delay: 3000,
-		disableOnInteraction: false,
-	},
-	// pagination: {
-	// 	el: "#blog-pag",
-	// 	clickable: true,
-	// },
-	navigation: {
-		nextEl: ".Nnext",
-		prevEl: ".Nprev", 
-	},
+  loop: true,
+  slidesPerView: 1,
+  direction: "vertical",
+  // spaceBetween: 0,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  // pagination: {
+  // 	el: "#blog-pag",
+  // 	clickable: true,
+  // },
+  navigation: {
+    nextEl: ".Nnext",
+    prevEl: ".Nprev",
+  },
 });
 // 滑鼠滑入後停止輪播
 $(".blog-banner").mouseenter(function () {
-	Bswiper.autoplay.stop();
+  Bswiper.autoplay.stop();
 });
 $(".blog-banner").mouseleave(function () {
-	Bswiper.autoplay.start();
+  Bswiper.autoplay.start();
 });
 
-// $(".close-btn").click(function(){
-// 	$("#ModalAdd").css("display","none");
-// 	$(".modal-backdrop").hide();
-// })
+// go to top
+  $(".goto-top").click(function () { 
+    $("html,body").animate({scrollTop:0},parseInt(300));
+  });
